38c5e29be0c94213249281964d908902637877d8
Markus <mkalusche@me.com>
2017-12-11 13:09:35 +0100

    first commit

---
+++ pytum.py
@@ -0,0 +1,11 @@
+"""
+PyTum Game
+from EPR-Job No.5
+"""
+#import nadda
+__author__ = "6598273: Markus Kalusche, 6768647: Tobias Denzer"  # your data
+__copyright__ = "Copyright 2017/2018 – Tobias Denzer & Markus Kalusche \
+                @ EPR-Goethe-Uni"
+__credits__ = "nobody"
+__email__ = "s1539940@stud.uni-frankfurt.de"
+
